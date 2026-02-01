5ab2d80d900b6816eeb303a57f6f6ded1327285a
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-02-01 10:23:35 -0800

    Week6-DRF_API-

---
+++ students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    course = models.CharField(max_length=50)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
